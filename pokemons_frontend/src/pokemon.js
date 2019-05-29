042dfa83864921b28a975a6bc709ca0c1f09736d
github username <github email address>
2019-05-29 16:07:01 -0500

    hello

---
+++ pokemons_frontend/src/pokemon.js
@@ -0,0 +1,24 @@
+const pokemonURL = "http://localhost:3000/pokemon"
+const div = document.querySelector(".pokemon_list")
+
+fetch(pokemonURL)
+  .then(res => res.json())
+  .then((data) => {
+    data.forEach((obj) => {
+renderObj(obj)
+    })
+  })
+
+  function renderObj(obj) {
+let ul = document.createElement("ul")
+let li = document.createElement("li")
+div.append(ul)
+
+li.innerText = obj.name
+ul.append(li)
+
+}
+
+
+
+
