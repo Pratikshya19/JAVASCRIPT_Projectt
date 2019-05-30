a5f63cd61bacce36c4ca37a67d95eb817cef9d53
github username <github email address>
2019-05-30 13:49:55 -0500

    thursday

--- pokemons_frontend/src/pokemon.js
+++ pokemons_frontend/src/pokemon.js
@@ -1,6 +1,10 @@
 const pokemonURL = "http://localhost:3000/pokemon"
 const div = document.querySelector(".pokemon_list")
 
+
+
+
+
 fetch(pokemonURL)
   .then(res => res.json())
   .then((data) => {
--- pokemons_frontend/src/trainer.js
+++ pokemons_frontend/src/trainer.js
@@ -1,18 +1,79 @@
-const trainerURL = "http://localhost:3000/trainer"
-
+// const trainerURL = "http://localhost:3000/trainer"
+const POKEMONS_URL = "http://localhost:3000/pokemon"
 
 fetch(trainerURL)
   .then(res => res.json())
   .then((data) => {
     data.forEach((obj) => {
+      console.log(obj)
 renderObj(obj)
+
     })
   })
 
   function renderObj(obj) {
-    let div = document.createElement("div");
-  div.setAttribute("class","card");
+
+
+    let cardDiv = document.createElement("div");
+    document.querySelector("main").append(cardDiv);
+    cardDiv.className = "card";
+    cardDiv.setAttribute("data-id", obj.id);
 
   let htag = document.createElement("h4");
-  h4tag.innerText = obj.name;
+  htag.innerText = obj.name;
+
+ 
+let addButton = document.createElement("button");
+addButton.setAttribute("data-trainer-id", obj.id);
+addButton.innerText = "Add Pokemon";
+
+    addButton.addEventListener("click", function(e) {
+      e.preventDefault()
+      debugger
+    })
+//       let id = parseInt(e.target.getAttribute("data-trainer-id"));
+//       fetch(POKEMONS_URL, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'Accept': 'application/json'
+//         },
+//         body: JSON.stringify({
+//           "trainer_id": id
+//         })
+//       }).then( (res) => res.json() )
+//       .then ( (pokemon) => {
+//         if(!pokemon.error) {
+//           // let ul = document.querySelector(`ul#trainer-${id}`);
+// let ul = document.querySelector("#pokemon")
+//           let li = document.createElement("li");
+//           ul.append(li);
+//           let button = document.createElement('button');
+//           li.innerText = `${pokemon.name}`;
+//           li.append(button);
+//           button.className = "DELETE";
+//           button.setAttribute('data-pokemon-id', pokemon.id);
+//         button.innerText = "delete";
+        
+        
+//         button.addEventListener("click", function(e){
+//           let id = e.target.getAttribute("data-pokemon-id");
+//           let r = fetch(`${POKEMONS_URL}/${id}`, {
+//             method: "DELETE" 
+//           }).then( console.log );
+//           e.target.parentElement.style.display = "none";
+//         });
+//       }
+
+
+//       });
+//     });
+
+
+
+
+
+ 
+
+
   }
